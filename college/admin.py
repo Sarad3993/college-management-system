5b203956124a042a7c7b217881827609df85ee5c
Sarad3993 <risingstar3993@gmail.com>
2022-11-18 11:33:34 +0545

    created models student and teacher

--- college/admin.py
+++ college/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from college.models import *
 
 # Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Teacher,TeacherAdmin)
--- college/models.py
+++ college/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+faculties = [('BSc. CSIT', 'BSc. CSIT'),('BIT','BIT'),('BSc.','BSc.'),('MSc.','MSc.')]
+class Student(models.Model):
+    user= models.OneToOneField(User,on_delete=models.CASCADE)
+    roll= models.CharField(max_length=10)
+    phone_no = models.CharField(max_length=50,blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    faculty= models.CharField(max_length=10,choices=faculties,default='Bsc. CSIT')
+    fee=models.PositiveIntegerField(blank=True)
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.first_name
+    
+    @property
+    def get_name(self):
+        return self.user.first_name + " " + self.user.last_name
+    
+    @property
+    def get_id(self):
+        return self.user.id
+    
+    
+    
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    salary= models.PositiveIntegerField(blank=False)
+    joindate = models.DateField(auto_now_add=True)
+    phone_no = models.CharField(max_length=50,blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    status = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.first_name 
+    
+    @property
+    def get_id(self):
+        return self.user.id
+    
+    @property
+    def get_name(self):
+        return self.user.first_name + " " + self.user.last_name
+    
+
+    
+    
