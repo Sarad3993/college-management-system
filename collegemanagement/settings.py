bd2b68a3147c8e538fbe7e62909280c16d8890ec
Sarad3993 <risingstar3993@gmail.com>
2022-11-18 13:02:49 +0545

    load static files and templates

--- collegemanagement/settings.py
+++ collegemanagement/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "collegemanagement.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR,'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+LOGIN_REDIRECT_URL='/afterlogin'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
